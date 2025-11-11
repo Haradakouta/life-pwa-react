a4a9c22c958825cd013401e5cbb88a3085b394d2
Haachan <refqyed@gmail.com>
2025-11-11 09:53:29 +0900

    chore: switch hosting to firebase

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/life-pwa-react/',
+  base: '/',
   build: {
     // コード分割の最適化
     rollupOptions: {
